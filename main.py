74a7c05cf77711082788c29d4a9c6eb15cb0656f
naum_tomov <n.tomov@student.utwente.nl>
2020-12-20 00:13:06 +0200

    work in progress

--- main.py
+++ main.py
@@ -1,92 +1,53 @@
-import numpy as np
-from mlxtend.data import loadlocal_mnist
+import tensorflow as tf
 
-#Importing dataset
-X, y = loadlocal_mnist(images_path='./data/train-images-idx3-ubyte', labels_path='./data/train-labels/idx1-ubyte')
-#Spliting dataset
-num_train = 50000
-num_test = 10000
-X_train = X[:num_train, :]/255
-y_train = np.zeros((num_train, 10))
-y_train[np.arange(0, num_train) ,y[:num_train]] = 1
-X_test = X[num_train:, :]/255
-y_test = np.zeros((num_test, 10))
-y_test[np.arange(0, num_test) ,y[y.size - num_test:]] = 1
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+import matplotlib.pyplot as plt
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+image_index = 7777  # You may select anything up to 60,000
+print(y_train[image_index])  # The label is 8
+plt.imshow(x_train[image_index], cmap='Greys')
 
-
-def sigmoid_derivative(x):
-    return x * (1 - x)
-
-
-def loss(predicted_output, desired_output):
-    return 1 / 2 * (desired_output - predicted_output) ** 2
+# Reshaping the array to 4-dims so that it can work with the Keras API
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
+# Converting values to floats so we can get decimal points after division
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+# Normalizing the RGB codes by dividing it to the max RGB value.
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print('Number of images in x_train', x_train.shape[0])
+print('Number of images in x_test', x_test.shape[0])
 
 
-class NeuralNetwork():
-    def __init__(self, inputLayerNeuronsNumber, hiddenLayerNeuronsNumber, outputLayerNeuronsNumber):
-        self.inputLayerNeuronsNumber = inputLayerNeuronsNumber
-        self.hiddenLayerNeuronsNumber = hiddenLayerNeuronsNumber
-        self.outputLayerNeuronsNumber = outputLayerNeuronsNumber
-        self.learning_rate = 0.1
-        # He initialization
-        self.hidden_weights = np.random.randn(hiddenLayerNeuronsNumber, inputLayerNeuronsNumber) * np.sqrt(
-            2 / inputLayerNeuronsNumber)
-        self.hidden_bias = np.zeros([hiddenLayerNeuronsNumber, 1])
-        self.output_weights = np.random.randn(outputLayerNeuronsNumber, hiddenLayerNeuronsNumber)
-        self.output_bias = np.zeros([outputLayerNeuronsNumber, 1])
-        self.loss = []
-
-    def train(self, inputs, desired_output):
-        hidden_layer_in = np.dot(self.hidden_weights, inputs) + self.hidden_bias
-        hidden_layer_out = sigmoid(hidden_layer_in)
-
-        output_layer_in = np.dot(self.output_weights, hidden_layer_out) + self.output_bias
-        predicted_output = sigmoid(output_layer_in)
-
-        error = desired_output - predicted_output
-        d_predicted_output = error * sigmoid_derivative(predicted_output)
-
-        error_hidden_layer = d_predicted_output.T.dot(self.output_weights)
-        d_hidden_layer = error_hidden_layer.T * sigmoid_derivative(hidden_layer_out)
-
-        self.output_weights += hidden_layer_out.dot(d_predicted_output.T).T * self.learning_rate
-        self.output_bias += np.sum(d_predicted_output, axis=0, keepdims=True) * self.learning_rate
-
-        self.hidden_weights += inputs.dot(d_hidden_layer.T).T * self.learning_rate
-        self.hidden_bias += np.sum(d_hidden_layer, axis=0, keepdims=True) * self.learning_rate
-        self.loss.append(loss(predicted_output, desired_output))
-
-    def predict(self, inputs):
-        hidden_layer_in = np.dot(self.hidden_weights, inputs) + self.hidden_bias
-        hidden_layer_out = sigmoid(hidden_layer_in)
-        output_layer_in = np.dot(self.output_weights, hidden_layer_out) + self.output_bias
-        predicted_output = sigmoid(output_layer_in)
-        return predicted_output
+def custom_mean_squared_error(y_true, y_pred):
+    return tf.math.reduce_mean(tf.square(y_true - y_pred))
 
 
-nn=NeuralNetwork(784,350,10)
+# Importing the required Keras modules containing model and layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 
-for i in range(X_train.shape[0]):
-    inputs = np.array(X_train[i, :].reshape(-1,1))
-    desired_output = np.array(y_train[i, :].reshape(-1,1))
-    nn.train(inputs, desired_output)
+# Creating a Sequential Model and adding the layers
+model = Sequential()
+model.add(Conv2D(28, kernel_size=(3, 3), input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())  # Flattening the 2D arrays for fully connected layers
+model.add(Dense(32, activation=tf.nn.relu))  # adds a layer with 32 neurons
+model.add(Dense(16, activation=tf.nn.relu))  # adds a layer with 16 neurons
+model.add(Dropout(0.2))
+model.add(Dense(10, activation=tf.nn.softmax))
 
-prediction_list = []
-for i in range(X_test.shape[0]):
-    inputs = np.array(X_test[i].reshape(-1, 1))
-    prediction_list.append(nn.predict(inputs))
+model.compile(optimizer='adam',
+              loss=custom_mean_squared_error,
+              metrics=['accuracy', 'mse'])
+y_train_one_hot = tf.one_hot(y_train, depth=10)
+model.fit(x=x_train, y=y_train_one_hot, epochs=5)
 
-correct_counter = 0
-for i in range(len(prediction_list)):
-    out_index = np.where(prediction_list[i] == np.amax(prediction_list[i]))[0][0]
-
-    if y_test[i][out_index] == 1:
-        correct_counter += 1
-
-accuracy = correct_counter / num_test
-
-print("Accuracy is : ", accuracy * 100, " %")
+predictions = model.predict(x_test[:3])
+print(predictions)
+y_test_one_hot = tf.one_hot(y_test, depth=10)
+print(model.evaluate(x_test, y_test_one_hot))
